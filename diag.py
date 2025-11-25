6db19ea7814fe2199fd77aeb8388db10c4eeb501
antonz123 <aikidoanton@gmail.com>
2025-11-25 20:50:52 +0300

    Update diag.py

--- diag.py
+++ diag.py
@@ -1,17 +1,21 @@
+import math
+
+
 with open('sequence.txt', 'r', encoding='utf-8') as f:
-    a = [float(i) for i in f]
+    numbers = [float(line) for line in f]
 
 CSI = '\x1B['
 RESET = f'{CSI}0m'
 
 
-def column(num, gr2):
-    print(f'{' '*(3-len(str(num)))}{num}|    {CSI}47m  {RESET}    {CSI}47m{"  " * gr2}{RESET}')
+def print_column(num, gr2):
+    print(f'{" " * (3 - len(str(num)))}{num}|    {CSI}47m  {RESET}    '
+          f'{CSI}47m{"  " * gr2}{RESET}')
 
 
-chet = sum(abs(a[i]) for i in range(0, len(a), 2))
-nechet = sum(abs(a[i]) for i in range(1, len(a), 2))
-gr2_num = nechet * 100 // chet
+even_sum = sum(abs(numbers[i]) for i in range(0, len(numbers), 2))
+odd_sum = sum(abs(numbers[i]) for i in range(1, len(numbers), 2))
+gr2_num = odd_sum * 100 // even_sum
 
 
 def paint():
@@ -20,7 +24,7 @@ def paint():
     for i in range(21):
         if num <= gr2_num:
             gr2 = 1
-        column(num, gr2)
+        print_column(num, gr2)
         num -= 5
     print('         Чёт  Нечёт')
 
