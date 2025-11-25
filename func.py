d9ff5487af484b79a0887bdf1c6f0b882d0f8f38
antonz123 <aikidoanton@gmail.com>
2025-11-25 20:52:46 +0300

    Update func.py

--- func.py
+++ func.py
@@ -2,22 +2,22 @@ CSI = '\x1B['
 RESET = f'{CSI}0m'
 
 
-def graph(width, num):
+def print_graph(width, num):
     print(
-        f'{" "*(2-len(str(num)))}{num} |{CSI}47m{" " * width}{CSI}42m{" "}{CSI}47m{" " * (10 + 10 - width)}{RESET}'
+        f'{" " * (2 - len(str(num)))}{num} |{CSI}47m{" " * width}'
+        f'{CSI}42m{" "}{CSI}47m{" " * (20 - width)}{RESET}'
     )
 
 
 def paint(width, num):
     for i in range(1, 12):
-        graph(width, num)
+        print_graph(width, num)
         width -= 1
         num -= 1
     print('    0 1 2 3 4 5')
 
 
-width = 10
-num = 10
-
 if __name__ == '__main__':
+    width = 10
+    num = 10
     paint(width, num)
